877d83cf28791ba6b9cc0e713593aab3dc545545
muharyman <muharymank@gmail.com>
2019-06-03 03:29:19 +0700

    newsForm

---
+++ frontend/src/component/NewsForm.js
@@ -0,0 +1,15 @@
+import React , {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+class NewsFrom extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default NewsForm;
